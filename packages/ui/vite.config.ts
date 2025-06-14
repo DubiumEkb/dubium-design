4a4dff3edc4bd1a38341fb560fdba99742899b31
DubiumEkb <dubium-ekb@yandex.ru>
2025-06-14 17:34:55 +0500

    Test Build Storybook

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,31 +1,41 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import dts from 'vite-plugin-dts';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import path from "path";
+import postcss from "rollup-plugin-postcss";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MyUILibrary',
-      fileName: (format) => `index.${format}.js`, // без "dd." префикса
-      formats: ['es', 'umd'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    cssCodeSplit: true,
-  },
+	plugins: [
+		react(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.ts"),
+			name: "DubiumUI",
+			fileName: (format) => `index.${format}.js`,
+			formats: ["es", "umd"],
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+			plugins: [
+				postcss({
+					modules: true,
+					inject: true,
+					extract: false,
+					minimize: true,
+					sourceMap: false,
+				}),
+			],
+		},
+		sourcemap: true,
+	},
 });
