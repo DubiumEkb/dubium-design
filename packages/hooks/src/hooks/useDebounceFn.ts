1ac8500147c1f96a7979ce9265eb4d58ef637342
Михаил Вострокнутов <dubium-ekb@yandex.ru>
2025-09-17 01:11:35 +0500

    Create useDebounceFn.ts
    Added useDebounceFn.ts

---
+++ packages/hooks/src/hooks/useDebounceFn.ts
@@ -0,0 +1,39 @@
+import { useCallback, useRef } from "react";
+
+/**
+	* Хук для создания дебаунсированной функции
+	* @param delay Задержка в миллисекундах
+	* @returns Функция, которая принимает callback для выполнения с задержкой
+	* 
+	* @example
+	* const debounce = useDebounceFn(300);
+	* 
+	* // Использование
+	* debounce(() => {
+	*   console.log('Выполняется после 300ms задержки');
+	* });
+*/
+export const useDebounceFn = (delay: number): ((callback: () => void) => void) => {
+	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+	// Очистка при размонтировании
+	useEffect(() => {
+    	return () => {
+      		if (timeoutRef.current) {
+        		clearTimeout(timeoutRef.current);
+      		}
+    	};
+  	}, []);
+
+  	return useCallback((callback: () => void) => {
+    	// Очищаем предыдущий таймер
+    	if (timeoutRef.current) {
+      		clearTimeout(timeoutRef.current);
+    	}
+
+    	// Устанавливаем новый таймер
+    	timeoutRef.current = setTimeout(() => {
+      		callback();
+    	}, delay);
+  	}, [delay]);
+};
