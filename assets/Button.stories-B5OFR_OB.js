627881d8eb0a48a279c08026e8d9c75b92e1bdc9
DubiumEkb <DubiumEkb@users.noreply.github.com>
2025-09-16 20:14:36 +0000

    deploy: 12e83c527878476746f9192439e480472b6c9b11

---
+++ assets/Button.stories-B5OFR_OB.js
@@ -0,0 +1,25 @@
+import{B as t}from"./Button-sP-X7Hpy.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-oBMKabMz.js";const{fn:n}=__STORYBOOK_MODULE_TEST__,p={title:"Example/Button",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:n()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}},o={args:{primary:!1,label:"Button"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...o.parameters?.docs?.source}}};const u=["Primary","Secondary","Large","Small","Qwerty"];export{e as Large,r as Primary,o as Qwerty,a as Secondary,s as Small,u as __namedExportsOrder,p as default};
